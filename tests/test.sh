c19958d3b428e01be826c8d36ef5abf5967f7a10
Ohohcakester <ohohgit@gmail.com>
2015-10-10 07:12:54 +0800

    Add tests

---
+++ tests/test.sh
@@ -0,0 +1,10 @@
+cp ../lal.py .
+python lal.py rendertest.tex
+#python lal.py -wide rendertest.tex
+#python lal.py -narr rendertest.tex
+#python lal.py -3col rendertest.tex
+#python lal.py -2col rendertest.tex
+#python lal.py -2colw rendertest.tex
+python lal.py -clean
+rm lal.py
+
